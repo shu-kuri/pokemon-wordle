5f0160b7d2fe1ffed7944cddb5098509cb15e420
Shuya-Kurihara <shu-kuri@nttpc.co.jp>
2025-05-01 18:50:36 +0900

    Update create_message.py
    正解例：
    ・入力内容表示
    ・完全一致判定
    ・部分一致判定
    ・カタカナのみ入力
    ・遊び方ボタン内容拡充
    ・ヒントボタンの内容拡充
    ・正解表示ボタンの内容拡充
    ・リセットボタンの内容拡充
    ・デバックモードの作成
    ・カウント処理実装
    
    未実装：
    ・正解ランダム選択
    ・正解ポケモンのタイプ指定
    ・タイムアタックモード実装
    ・vsオプション実装

--- create_message.py
+++ create_message.py
@@ -4,85 +4,103 @@ import re
 
 with open("pokemon_list.csv", "r", encoding="utf-8") as f:
         r = csv.reader(f)
-        pokemons = [row for row in r]
+        pokemons_list = [row for row in r]
 num = 0
-pokemon ='test'
+pokemon ='ピカチュウ'
 taipu = ["でんき","なし"]
+type1 = "指定なし"
+type2 = "指定なし"
 
-def select():
-    global pokemon, taipu
-    pokemon = pokemons[random.randint(0,276)]
-    pokemon = pokemon[0]
-select()
-#print(pokemon)
-
-def call_ai():
-    ai_poke = pokemons[random.randint(0,276)]
-    ai_poke_name = ai_poke[0]
-    ai_poke_type1 = ai_poke[1]
-    ai_poke_type2 = ai_poke[2]
-    return ai_poke_name
-#print(call_ai())
-
-def is_katakana(text):
-    pattern = r'^[ァ-ヶー]+$'
-    if re.match(pattern, text):
-        return True
-    else:
-        return False
-
-def hantei(user_input,debug_flag):
-    global num,pokemon
-    num += 1
-    if user_input == pokemon:
-        message = '{}手目で正解!!!'.format(num) + '\nおめでとう！'
-    elif is_katakana(user_input) != True:
-        num -= 1
-        message = 'ポケモン名は全角カタカナだよ'
-    else:
-        message ='ハズレ!!!' + '\nまた挑戦してね！' 
-        #message = pokemon
-    if debug_flag == True:
-        message = "valid is {}: ".format(pokemon) + message
+def select(taipu1, taipu2):
+    message = str(pokemons_list[0][0])
     return message
 
-def reset_num():
+select(type1,type2)
+
+def call_ai():
+    pass
+
+def is_katakana(text,messaged):
+    p = re.compile('[\u30a1-\u30fa\u30fc]+')
+
+    if not p.fullmatch(text):
+        message ="入力はカタカナのみです。"
+        return message
+
+    else:
+        return messaged
+
+
+def hantei(user_input):
     global num
+    global pokemon
+    if len(user_input) == 5:
+        num += 1
+        if user_input == pokemon:
+            content = "正解！<br>"
+            message =content+ str(num) + "手目で正解！"
+        else:
+            content = "残念！<br>"
+            message =content + "間違えです。"
+        for i in range(len(user_input)):
+            if user_input[i] == pokemon[i]:
+                message += '<div class="text-white bg-success d-inline">' + user_input[i] + '</div>'
+            else:
+                message += '<div class="text-white bg-secondary d-inline">' + user_input[i] + '</div>'
+    else:
+        message = "5文字の文字を入力してください。"
+    message = is_katakana(user_input,message)
+    return message
+
+def debug_message(user_input, request):
+    global pokemon
+    message=[]
+    message.append("───デバッグモード───<br>")
+    message.append("user_input:"+request['user_input'])
+    message.append("option1:"+request['options1'])
+    message.append("option2:"+request['options2'])
+    message.append("debug:"+request['debug'])
+    message.append("current_message:"+request['current_message'])
+    message.append(pokemon)
+    message.append("")
+    message.append("─デバッグモード─")
+
+    full_text=""
+    for str in message:
+        full_text+=str + "<br>"
+
+    return full_text
+
+def reset(taipu1, taipu2):
+    global num
+    global pokemon
+    global taipu
+    global type1
+    global type2
     num = 0
-    message = "リセット完了"
-    select()
+    pokemon ='ピカチュウ'
+    taipu = ["でんき","なし"]
+    type1 = "指定なし"
+    type2 = "指定なし" 
+    message = "リセットしました。<br>"
+    select(taipu1,taipu2)
     return message
 
 def hint():
-    message = "helpモード\nTEST"
+    message = "test"
     return message
 
-def vs(user_input, debug_flag):
-    global num,pokemon
-    ai_poke = call_ai()
-    user_result = hantei(user_input, debug_flag)
-    if "おめでとう" in user_result:
-        message = user_result
-    elif ai_poke == pokemon:
-        message = "{}手目でAIの勝ち！！正解は {}".format(num,pokemon)
-    else:
-        message = "はずれ！！　User:" + user_input + " AI:" + ai_poke
-        if debug_flag == True:
-            message = "valid is {}: ".format(pokemon) + message
-    if is_katakana(user_input) != True:
-        num -= 1
-        message = 'ポケモン名は全角カタカナだよ'
- #   message = "vs mode"
+def vs(user_input):
+    message = "test"
     return message
 
-
-def ai(user_input, debug_flag):
-    message = "AIモード"
+def helps():
+    title = "＊遊び方＊<br>"
+    explains ="入力欄からポケモンの名前を予測して入力→送信<br>"
+    tips = "[機能一覧]<br>・ヒント:ポケモンのタイプがわかるよ<br>・正解表示:答えがわかるよ<br>・リセット:正解をリセットするよ<br>・二つのプルダウンメニューからポケモンのタイプを絞れるよ"
+    message = title+explains+tips
     return message
 
-
-#Debug command
-#for i in range(5):
-#    print(hantei("ピカチュウ",True))
-#print(hantei("TEST",True))
-#print(hantei(pokemon,True))
+def answer():
+    message = pokemon
+    return message

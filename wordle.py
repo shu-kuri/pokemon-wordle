c61c826bb859a8960f5441b5c4c63746828c1162
Shuya Kurihara <shu-kuri@nttpc.co.jp>
2025-05-15 18:31:11 +0900

    Update wordle.py
    コメントアウトでコードの補足追記

--- wordle.py
+++ wordle.py
@@ -1,6 +1,8 @@
+# Flaskと必要なモジュールをインポート
 from flask import Flask, request, render_template
 from markupsafe import escape
 
+# 外部モジュール create_message から関数をインポート（存在しない場合は無視）
 try:
     from create_message import hantei
 except:
@@ -36,10 +38,13 @@ try:
 except:
     pass
 
+# Flaskアプリケーションのインスタンスを作成
 app = Flask(__name__)
 
+# ルートURLにアクセスしたときの処理（GETとPOSTの両方に対応）
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    # 初期化：フォームの入力やメッセージなどの変数
     user_input = ''
     current_message = ''
     message = ''
@@ -47,12 +52,19 @@ def home():
     selected_option2 = ''
     debug = False
     vsai_flag = False
+
+    # タイプ選択肢（ポケモンのタイプ）
     options1 = ['指定なし', 'ノーマル', 'ほのお', 'みず', 'でんき', 'くさ', 'こおり', 'かくとう', 'どく', 'じめん', 'ひこう', 'エスパー', 'むし', 'いわ', 'ゴースト', 'ドラゴン','あく', 'はがね']
     options2 = options1
+
+    # POSTリクエスト時の処理
     if request.method == 'POST':
+        # 現在のメッセージとユーザー入力を取得
         message = request.form.get('current_message', '')
         user_input_raw = request.form.get('user_input', '')
-        user_input = str(escape(user_input_raw))
+        user_input = str(escape(user_input_raw))  # HTMLエスケープで安全に
+
+        # 各ボタンに応じた処理を実行
         if 'debug' in request.form:
             message += debug_message(user_input, request.form) + "<br>"
         if 'vsai' in request.form:
@@ -64,15 +76,30 @@ def home():
         elif 'answer' in request.form:
             message += answer() + "<br>"
         elif 'reset' in request.form:
+            # リセット時は選択肢を取得してメッセージを初期化
             selected_option1 = request.form.get('options1')
             selected_option2 = request.form.get('options2')
             current_message = ''
             message = reset(selected_option1, selected_option2)
         else:
+            # 通常の判定処理
             user_input_raw = request.form.get('user_input', '')
             user_input = str(escape(user_input_raw))
             message += hantei(user_input) + "<br>"
-    return render_template('index.html', user_input=user_input, message=message, current_message=current_message, debug=debug, options1=options1, options2=options2, selected_option1=selected_option1, selected_option2=selected_option2)
 
+    # テンプレートに変数を渡してHTMLをレンダリング
+    return render_template(
+        'index.html',
+        user_input=user_input,
+        message=message,
+        current_message=current_message,
+        debug=debug,
+        options1=options1,
+        options2=options2,
+        selected_option1=selected_option1,
+        selected_option2=selected_option2
+    )
+
+# アプリケーションをデバッグモードで起動
 if __name__ == "__main__":
     app.run(debug=True)
